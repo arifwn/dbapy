82322cb90139ed667a7d625644f6c296ee1f3127
Arif Widi Nugroho <arif@sainsmograf.com>
2014-03-25 21:20:34 +0700

    added ember.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,15 @@
 (function() {
 
+  App = Ember.Application.create();
+
+  App.Router.map(function() {
+    // put your routes here
+  });
+
+  App.IndexRoute = Ember.Route.extend({
+    model: function() {
+      return ['red', 'yellow', 'blue'];
+    }
+  });
+
 }).call(this)
